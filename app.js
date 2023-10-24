26a94dd165185aa468daa747df9468a5a7e818ee
lumberlark <slark20@gmail.com>
2023-10-23 22:54:16 -0500

    Adjusted buyButton to say Show now, edited productDesc to fit better with size of screen. Adjusted images and custom sizes using GIMP for gallery section. Also added images to collection section along with custom sizes. Finished editing mobile responsivness as well.

--- app.js
+++ app.js
@@ -4,27 +4,42 @@ const menuItems = document.querySelectorAll(".menuItem");
 const products = [
     {
       id: 1,
-      title: "Classic",
-      price: 160,
-      colors: [
-        {
-          code: "gold",
-          img: "./img/classic.png",
-        },
-        {
-          code: "royalblue",
-          img: "./img/classic-blue.png",
-        },
-      ],
-    },
+    title: "Aviator",
+    price: '171.00',
+    descriptions: [
+      "<b>FRAME SHAPE:</b> Pilot",
+      "<b>COLOR:</b> Polished Gold",
+      "<b>MATERIAL:</b> Metal",
+      "<b>TEMPLE COLOR:</b> Gold",
+      "<b> LENS:</b> Black | Blue"
+    ],
+    colors: [
+      {
+        code: "black",
+        img: "./img/classic.png",
+      },
+      {
+        code: "skyblue",
+        img: "./img/classic-blue.png",
+      },
+    ],
+  },
     {
       id: 2,
       title: "Wayfarer",
       price: 160,
+      descriptions: [
+        "<b>FRAME SHAPE:</b> Square",
+        "<b>COLOR:</b> Polished Black | Havana",
+        "<b>MATERIAL:</b> Acetate",
+        "<b>TEMPLE COLOR:</b> Black | Havana",
+        "<b> LENS:</b> Black | Orange"
+      ],
       colors: [
         {
           code: "black",
           img: "./img/wayfarer.png",
+          txt: "more text should be here",
         },
         {
           code: "orange",
@@ -36,13 +51,20 @@ const products = [
       id: 3,
       title: "Bill",
       price: 109,
+      descriptions: [
+        "<b>FRAME SHAPE:</b> Square",
+        "<b>COLOR:</b> Polished Blue On Transparent",
+        "<b>MATERIAL:</b> Acetate",
+        "<b>TEMPLE COLOR:</b> Blue | Leopard",
+        "<b> LENS:</b> Dark Gray | Tortoise"
+      ],
       colors: [
         {
-          code: "silver",
+          code: "gray",
           img: "./img/bill-white.png",
         },
         {
-          code: "brown",
+          code: "tortoise",
           img: "./img/bill-lepord.png",
         },
       ],
@@ -51,6 +73,13 @@ const products = [
       id: 4,
       title: "Caravan",
       price: 200,
+      descriptions: [
+        "<b>FRAME SHAPE:</b> Square",
+        "<b>COLOR:</b> Polished Silver | Black",
+        "<b>MATERIAL:</b> Metal",
+        "<b>TEMPLE COLOR:</b> Silver | Black",
+        "<b> LENS:</b> Clear"
+      ],
       colors: [
         {
           code: "silver",
@@ -66,13 +95,20 @@ const products = [
       id: 5,
       title: "ClubMaster",
       price: 171,
+      descriptions: [
+        "<b>FRAME SHAPE:</b> Square",
+        "<b>COLOR:</b> Polished Red Havana | Black",
+        "<b>MATERIAL:</b> Acetate",
+        "<b>TEMPLE COLOR:</b> Red Havana | Black",
+        "<b> LENS:</b> Green | Light Green"
+      ],
       colors: [
         {
-          code: "brown",
+          code: "green",
           img: "./img/clubmaster.png",
         },
         {
-          code: "gold",
+          code: "lightgreen",
           img: "./img/clubemaster-gold.png",
         },
       ],
@@ -85,6 +121,7 @@ const products = [
   const currentProductTitle = document.querySelector(".productTitle");
   const currentProductPrice = document.querySelector(".productPrice");
   const currentProductColors = document.querySelectorAll(".color");
+  const currentProductText = document.querySelector(".textList");
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
@@ -113,8 +150,36 @@ currentProductColors.forEach((color,index) => {
   })
 });
 
+// Select the product description element
+const productDesc = document.querySelector(".productDesc");
 
+// Update product details and description for the initial product (first in the array)
+function updateProductDetails(index) {
+  choosenProduct = products[index];
 
+  currentProductTitle.textContent = choosenProduct.title;
+  currentProductPrice.textContent = "$" + choosenProduct.price;
+  currentProductImg.src = choosenProduct.colors[0].img;
+
+  if (choosenProduct.descriptions) {
+    productDesc.innerHTML = choosenProduct.descriptions.join("<br>");
+  } else {
+    productDesc.textContent = "No description available";
+  }
+}
+
+// Add an event listener for each menu item
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    wrapper.style.transform = `translate(${-100 * index}vw)`;
+    updateProductDetails(index);
+  });
+});
+
+// Initialize with the first product
+updateProductDetails(0);
+
+// Payment box that opens on Button click
 const productButton = document.querySelector(".productButton");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
